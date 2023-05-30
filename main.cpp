b6ec56502de0e07e15e2f4482dd3380b77e0203b
Rodrigo Gama <rodrigo1902.rb@gmail.com>
2023-05-29 22:08:24 -0300

    Added readme.md

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ struct Point {
     float y;
 };
 
+const sf::Color BACKGROUND_COLOR = sf::Color(150, 197, 247);
+const sf::Color LINE_COLOR = sf::Color(238, 229, 191);
+const sf::Color POINT_COLOR = sf::Color(105, 109, 125);
+const sf::Color SEGMENT_POINT_COLOR = sf::Color::White;
+
 class QuadraticBezierCurve {
 public:
 
@@ -19,21 +24,28 @@ public:
     sf::CircleShape  endCircle;
     sf::CircleShape  controlCircle;
 
+    float lineThickness;
+    sf::Color lineColor;
+
     QuadraticBezierCurve(const Point& startPoint, const Point& endPoint, const Point& controlPoint, int numPoints)
         : start_point(startPoint), end_point(endPoint), control_point(controlPoint) {
             
         startCircle = sf::CircleShape(10);
-        startCircle.setFillColor(sf::Color::Yellow);
         startCircle.setOrigin(5, 5);
 
-        endCircle = sf::CircleShape(10);
-        endCircle.setFillColor(sf::Color::Blue);
-        endCircle.setOrigin(5, 5);
-
         controlCircle = sf::CircleShape(10);
-        controlCircle.setFillColor(sf::Color::Green);
         controlCircle.setOrigin(5, 5);
 
+        endCircle = sf::CircleShape(10);
+        endCircle.setOrigin(5, 5);
+
+        lineThickness = 5.0f;
+        
+        // Custom RGB Color
+        lineColor = sf::Color(LINE_COLOR);
+        startCircle.setFillColor(POINT_COLOR);
+        controlCircle.setFillColor(POINT_COLOR);
+        endCircle.setFillColor(POINT_COLOR);
     }
 
     std::vector<Point> generateCurve(int numPoints) {
@@ -56,34 +68,35 @@ public:
 
     void draw(sf::RenderWindow &window){
 
-        for (const auto& line : shapeLine) {
+        // Draw line
+        for (const auto& line : generateShapeLine(curvePoints)) {
             window.draw(line);
         }
 
+        // Draw Line Points
         for (const auto& point : curvePoints) {
             sf::CircleShape circle = sf::CircleShape(4);
-            circle.setFillColor(sf::Color::Red);
+            circle.setFillColor(SEGMENT_POINT_COLOR);
             circle.setOrigin(2, 2);
             circle.setPosition(point.x - 2, point.y - 2);
             window.draw(circle);
         }
 
+        // Draw Control Points
         window.draw(startCircle);
         window.draw(endCircle);
         window.draw(controlCircle);
-        
-
+    
     }
 
-    std::vector<sf::RectangleShape> generateShapeLine(const std::vector<Point>& points, float thickness) {
+    std::vector<sf::RectangleShape> generateShapeLine(const std::vector<Point>& points) {
+        // Creates an array of rectangles that form a line between the points
+
         std::vector<sf::RectangleShape> rectangles;
 
         if (points.size() < 2) {
-            // Not enough points to form a line
             return rectangles;
         }
-
-        sf::Color lineColor = sf::Color::White;  // Change the color as desired
 
         for (std::size_t i = 1; i < points.size(); ++i) {
             const Point& p1 = points[i - 1];
@@ -96,20 +109,19 @@ public:
             float dy = p2.y - p1.y;
             float length = std::sqrt(dx * dx + dy * dy);
 
-            rectangle.setSize(sf::Vector2f(length, thickness));
+            rectangle.setSize(sf::Vector2f(length, lineThickness));
             rectangle.setFillColor(lineColor);
 
             // Calculate the rotation angle
             float angle = std::atan2(dy, dx) * 180.0f / static_cast<float>(M_PI);
             rectangle.setRotation(angle);
-            rectangle.setOrigin(0.0f, thickness / 2.0f);
+            rectangle.setOrigin(0.0f, lineThickness / 2.0f);
 
             rectangles.push_back(rectangle);
         }
 
         return rectangles;
     }
-
 
     void updateCurve(const Point& startPoint, const Point& endPoint, const Point& controlPoint, int numPoints){
 
@@ -118,19 +130,11 @@ public:
         control_point = controlPoint;
 
         curvePoints = generateCurve(numPoints);
-
-        shapeLine = generateShapeLine(curvePoints, 5);
-
+    
         startCircle.setPosition(start_point.x - 5, start_point.y - 5);
         endCircle.setPosition(end_point.x - 5, end_point.y - 5);
         controlCircle.setPosition(control_point.x - 5, control_point.y - 5);
     }
-
-    void updateCurve(){
-        updateCurve(start_point, end_point, control_point, numPoints);
-    }
-
-
 
 private:
     int numPoints;
@@ -198,7 +202,7 @@ int main() {
 
         curve.updateCurve(start, end, control, numPoints);
 
-        window.clear();
+        window.clear(BACKGROUND_COLOR);
         curve.draw(window);
         window.display();
     }
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Quadratic Bezier Curve Application
+
+This is a C++ project that utilizes the SFML library to create a user-friendly application for manipulating quadratic Bezier curves. The application allows users to interactively control the shape of the curve by dragging control points and adjust the level of detail by scrolling up or down.
+
+## Features
+
+- **Quadratic Bezier Curve:** The application focuses on quadratic Bezier curves, which are defined by three control points - a start point, a control point, and an end point. These curves are widely used in computer graphics and animation.
+
+- **Draggable Control Points:** Users can click and drag the control points of the quadratic Bezier curve to dynamically change its shape. This feature enables intuitive manipulation of the curve and provides real-time visual feedback.
+
+- **Scroll Up/Down for Level of Detail:** By scrolling up or down, users can increase or decrease the number of line segments used to approximate the curve. This allows for adjusting the level of detail and smoothness of the curve, providing control over its visual appearance.
+
+## Dependencies
+
+To compile and run this application, you need to have the following dependencies:
+
+- **SFML Library:** The project utilizes the SFML library (Simple and Fast Multimedia Library) to handle window creation, event handling, and rendering. Make sure you have SFML properly installed on your system.
+
+## Usage
+
+1. Clone or download the project repository to your local machine.
+
+2. Open the project in your preferred C++ development environment.
+
+3. Make sure you have properly set up the SFML library with your project.
+
+4. Build and compile the project.
+
+5. Run the compiled executable.
+
+6. The application window will open, displaying the initial quadratic Bezier curve with control points.
+
+7. Click and drag the control points to manipulate the shape of the curve.
+
+8. Scroll up or down to increase or decrease the level of detail (number of line segments) used to approximate the curve.
+
+9. Explore different configurations and enjoy interacting with the quadratic Bezier curve.
+
+## Contributing
+
+Contributions to this project are welcome! If you find any bugs, have suggestions for improvements, or want to add new features, feel free to open an issue or submit a pull request. Please make sure to follow the project's code of conduct.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE.md). Feel free to use, modify, and distribute the code for personal or commercial purposes. However, please provide appropriate attribution to the original author.
